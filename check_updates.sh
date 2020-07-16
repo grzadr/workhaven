c19b0d0559340e4344cd0b21f4172adcc75d492f
Adrian Grzemski <adrian.grzemski@gmail.com>
2020-07-17 00:43:39 +0200

    upgraded script for checking updates

--- check_updates.sh
+++ check_updates.sh
@@ -5,5 +5,38 @@ set -o pipefail
 
 CONTAINER_NAME="workhaven_check_updates"
 
-docker run --name ${CONTAINER_NAME} --rm -it "grzadr/workhaven:${1:-latest}" condaup --dry-run
+docker run --name ${CONTAINER_NAME} --rm -it "grzadr/workhaven:${1:-latest}" condaup --dry-run --json | \
+grep -Pzo -e '\"LINK\"\:\s+\[([^\]]+)\]' | \
+head -n -1 | \
+tail -n +2 | \
+awk '
+BEGIN{
+  RS="\\s+},\\s+"; 
+  FS = "\n";
+}
+{
+    match($7, "\"name\":\\s+\"(\\S+)\"", matched);
+    name = matched[1];
+    
+    match($9, "\"version\":\\s+\"(\\S+)\"", matched);
+    version=matched[1];
+    
+    print name "=" version;
+    next;
+}
+' | awk '
+BEGIN{
+  RS = "\n"; 
+  FS = "=";
+}
+FNR==NR {
+  registry[$1] = $2
+  next;
+}
+/^#/{ print $0; next }
+!length($0) {print $0; next}
+{
+  if ($1 in registry){print $1 "=" registry[$1]} else {print $0}
+}
+' - packages/conda.list > packages/conda.list.new
 

803021665da7f7ad182838f16b98b48fd353ecd2
Adrian Grzemski <adrian.grzemski@gmail.com>
2020-07-21 15:51:36 +0200

    Script will now overwrite oryginal file and backup old version

--- check_updates.sh
+++ check_updates.sh
@@ -4,6 +4,10 @@ set -eux
 set -o pipefail
 
 CONTAINER_NAME="workhaven_check_updates"
+CONDA_LIST_OLD="packages/conda.list.old"
+CONDA_LIST="packages/conda.list"
+
+cat $CONDA_LIST > $CONDA_LIST_OLD
 
 docker run --name ${CONTAINER_NAME} --rm -it "grzadr/workhaven:${1:-latest}" condaup --dry-run --json | \
 grep -Pzo -e '\"LINK\"\:\s+\[([^\]]+)\]' | \
@@ -11,22 +15,22 @@ head -n -1 | \
 tail -n +2 | \
 awk '
 BEGIN{
-  RS="\\s+},\\s+"; 
+  RS="\\s+},\\s+";
   FS = "\n";
 }
 {
     match($7, "\"name\":\\s+\"(\\S+)\"", matched);
     name = matched[1];
-    
+
     match($9, "\"version\":\\s+\"(\\S+)\"", matched);
     version=matched[1];
-    
+
     print name "=" version;
     next;
 }
 ' | awk '
 BEGIN{
-  RS = "\n"; 
+  RS = "\n";
   FS = "=";
 }
 FNR==NR {
@@ -38,5 +42,5 @@ FNR==NR {
 {
   if ($1 in registry){print $1 "=" registry[$1]} else {print $0}
 }
-' - packages/conda.list > packages/conda.list.new
+' - $CONDA_LIST_OLD > $CONDA_LIST
 

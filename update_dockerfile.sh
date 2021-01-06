d550e7e87f095090557baaa259e0bf95ec187e80
Adrian Grzemski <adrian.grzemski@gmail.com>
2021-01-06 12:31:55 +0100

    Changed tag to digest

--- update_dockerfile.sh
+++ update_dockerfile.sh
@@ -5,6 +5,6 @@ set -o pipefail
 
 docker pull -q jupyter/minimal-notebook:latest > /dev/null
 
-NEW_TAG=$(docker images --format "{{.ID}}|{{.CreatedAt}}" | sort -t '|' -k 2 -r | head -n 1 | cut -d'|' -f 1)
+NEW_TAG=$(docker images --format "{{.Repository}}|{{.Digest}}|{{.CreatedAt}}" | grep "^jupyter/minimal-notebook|" | sort -t '|' -k 3 -r | head -n 1 | cut -d'|' -f 2)
 
-sed -i'.old' "s_FROM jupyter/minimal-notebook:[a-z0-9]*_FROM jupyter/minimal-notebook:${NEW_TAG}_" Dockerfile
+sed -i'.old' "s_FROM jupyter/minimal-notebook@sha256:[a-z0-9]*_FROM jupyter/minimal-notebook@${NEW_TAG}_" Dockerfile
